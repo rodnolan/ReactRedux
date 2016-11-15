7ebb343c85f8a6f05ad0ab1a09cc759a00259873
rodnolan <rodnolan@gmail.com>
2016-11-15 11:40:42 -0500

    binding 'this' in the event handlers since React doesn't autobind in ES6 classes

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -8,14 +8,26 @@ class CoursesPage extends React.Component {
     super(props, context);
 
     this.state = {
-      course: {title: null}
+      course: { title: '' }
     };
+
+
+    // React doesn't autobind in ES6 classes so you have to use bind to give 'this' the expected meaning
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+    // it's best for performance to bind in the constructor, not in render()... calling bind in render causes
+    // a new function to be created every time render() executes.
   }
 
   onTitleChange(event) {
     const course = this.state.course;
     course.title = event.target.value;
     this.setState({course: course});
+  }
+
+  onClickSave() {
+    // so that, in the expression below, this.state doesn't try to read 'state' from the input
+    alert(`Saving course: ${this.state.course.title}`);
   }
 
   render () {
@@ -28,7 +40,7 @@ class CoursesPage extends React.Component {
           onChange={this.onTitleChange}
           value={this.state.course.title}
         />
-
+        /* 'this' is the input and it's that object that becomes 'this' within the context of the onTitleChange function */
         <input
           type="submit"
           value="Save"
