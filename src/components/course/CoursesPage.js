d4dc54741f78eccb9357471832985c4cae863178
rodnolan <rodnolan@gmail.com>
2016-11-15 10:54:23 -0500

    adding just the markup for the form along with the constructor to initialise the component state

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,11 +4,36 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: {title: null}
+    };
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
   render () {
     return (
       <div>
         <h1>Courses</h1>
-        <p>well this is fun... when it works... which is never... hot reloading sucks</p>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}
+        />
       </div>
     );
   }
