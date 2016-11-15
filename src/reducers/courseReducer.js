114d631b28d0b881906f2be3b3ccf26937bfc67b
rodnolan <rodnolan@gmail.com>
2016-11-15 14:31:18 -0500

    adding the first version of the first reducer

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,15 @@
+export default function courseReducer(state = [], action) {
+  switch(action.type) {
+    case 'CREATE_COURSE':
+      return [...state,
+        Object.assign({}, action.course)
+      ];
+      break;
+
+    default:
+      return state;
+      break;
+
+  }
+  return {};
+}
